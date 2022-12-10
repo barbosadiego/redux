bbccdf45a0dd5ea0376c472ad65b1eb784cb555f
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-10 10:45:24 -0300

    add cart component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import Cart from './components/cart/Cart';
 import store from './store/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>App</div>
+      <Cart />
     </Provider>
   );
 };
