f851ca0e01265df89e50ec51e8b8cffeeb9e9f41
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-10 10:43:47 -0300

    create cart component

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyledCart, Button } from './styles';
+
+const Cart = () => {
+  return (
+    <StyledCart>
+      <h2>Number of items in Cart</h2>
+      <Button>Add item to Cart</Button>
+      <Button>Remove item to Cart</Button>
+    </StyledCart>
+  );
+};
+
+export default Cart;
