14070efeff0ca9f2684c928c341311be04ba3f84
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-11 10:15:11 -0300

    start new branch

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import cartReducers from '../reducers/cartReducers';
-
-const store = createStore(
-  cartReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-export default store;
